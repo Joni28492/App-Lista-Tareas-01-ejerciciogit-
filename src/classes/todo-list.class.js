ee8d52b338b30ed3bfda4bac62cd74dfe12c6ed7
Joni <joni28492@gmail.com>
2021-01-02 21:42:42 +0100

    primer commit

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,52 @@
+import { Todo } from "./todo.class";
+
+
+
+export class TodoList{
+
+    constructor(){
+       // this.todos=[];
+       this.cargarLocalStorage();
+    }
+    //agregamos el nuevo todo
+    nuevoTodo(todo){
+        this.todos.push(todo);
+        this.guardarLocalStorage();
+    }
+    eliminarTodo( id ){
+       this.todos = this.todos.filter(todo => todo.id != id);
+       this.guardarLocalStorage();
+    }
+
+    marcarCompletado(id){
+        for (const todo of this.todos) {
+            
+            if (todo.id == id) {
+                
+                todo.completado = !todo.completado;
+                this.guardarLocalStorage();
+                break;
+            }
+        }
+    }
+
+
+    eliminarCompeltados(){
+        this.todos = this.todos.filter(todo => !todo.completado );
+        this.guardarLocalStorage();
+    }
+
+    guardarLocalStorage(){
+        localStorage.setItem('todo',JSON.stringify(this.todos)  );
+    }
+    cargarLocalStorage(){
+
+        this.todos = (localStorage.getItem('todo')) 
+                    ? JSON.parse(localStorage.getItem('todo'))
+                    :[];
+
+        this.todos=this.todos.map( Todo.fromJson);
+
+    }
+
+}
---
+++ src/classes/todo.class.js
@@ -0,0 +1,27 @@
+
+
+export class Todo{
+
+    static fromJson({id, tarea, completado, creado}){
+
+        const tempTodo      = new Todo(tarea);
+        tempTodo.id         = id;
+        tempTodo.completado = completado;
+        tempTodo.creado     = creado;
+
+        return tempTodo;
+    }
+
+    constructor( tarea ){
+
+        this.tarea = tarea;
+        //creamos de forma auto lo sig
+        this.id         = new Date().getTime();//565421397
+        this.completado =false;
+        this.creado     =new Date();
+    }
+
+    imprimirClase(){
+        console.log(`${this.tarea} - ${this.id}`);
+    }
+}
